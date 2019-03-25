6ecced0a686aef309622ce063e982bd2d1bd25bc
lyxcious <felix_laurensius@yahoo.com>
2019-03-25 16:17:45 +0700

    update

---
+++ excercise-1.js
@@ -0,0 +1,51 @@
+//NUMBER 1//
+STORE "m" with 600;
+STORE "a" with 2;
+STORE "F" without any value;
+
+CALCULATE "m" multiple by "a";
+SET "F" value with calculation result;
+
+DISPLAY "F";
+
+//NUMBER 2//
+STORE "Year" with any value;
+
+IF "Year" mod 4 equal to 0;
+    IF "Year" mod 100 equal to 0;
+        IF "Year" mod 400 equal to 0;
+            DISPLAY "Leap Year";
+        ELSE;
+            DISPLAY "Not Leap Year";
+        END IF;
+    ELSE;
+        DISPLAY "Leap Year";
+    END IF;
+ELSE;
+    DISPLAY "Not Leap Year";
+END IF;
+
+//NUMBER 3//
+STORE "Laundry" with 0;
+
+WHILE "Laundry" less than 20;
+    DISPLAY "Total clothes entered is" plus " " plus "Laundry";
+    DISPLAY "Do not turn on the washing machine";
+    ADD "Laundry" by 1;
+END WHILE;
+
+DISPLAY "20 clothes is in, turn on the washing machine";
+
+//NUMBER 4//
+STORE "Student" with 0;
+
+WHILE "Student" less than 41;
+    DISPLAY "Student number" plus " " plus "Student";
+    DISPLAY "Student nail's long? true/false";
+    SET "LongNail" with true or false;
+    IF "LongNail" is true;
+        DISPLAY "Student get punished";
+    ELSE;
+        DISPLAY "Student get praised";
+    END IF;
+END WHILE;
